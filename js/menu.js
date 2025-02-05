3b2e699ce2813d38aa48b5e0886a33ee42880b83
arnold korir <arnoldkorir201@gmail.com>
2025-02-05 20:27:11 +0300

    added images to the menu page

---
+++ js/menu.js
@@ -0,0 +1,22 @@
+// Sample product data
+const products = [
+  {
+    id: 1,
+    name: "Fresh Tomatoes",
+    price: 150,
+    image: "https://images.unsplash.com/photo-1518977676601-b53f82aba655",
+  },
+  {
+    id: 2,
+    name: "Organic Carrots",
+    price: 120,
+    image: "https://images.unsplash.com/photo-1447175008436-054170c2e979",
+  },
+  {
+    id: 3,
+    name: "Watermelon",
+    price: 350,
+    image:
+      "https://dtgxwmigmg3gc.cloudfront.net/imagery/assets/derivations/icon/512/512/true/eyJpZCI6IjZhYTI3ZmUxYzliMTNiMWEyMTJlYTU5ZGQxNDMzZjc5Iiwic3RvcmFnZSI6InB1YmxpY19zdG9yZSJ9?signature=7c42ad0bf27606f1ae28afd728325428fdd5f9c9eb0bb07297ecbb00b145a3a9",
+  },
+];
